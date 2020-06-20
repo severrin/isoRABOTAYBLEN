fae6e7259eebf870b036efb50cf80bad46a5c379
toaddoesnot <laiscusn7@gmail.com>
2020-06-20 03:04:07 +0300

    Menu + i animations

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGameI()
+    {
+        SceneManager.LoadScene("GAMEi");
+    }
+
+    public void PlayGameSO()
+    {
+        SceneManager.LoadScene("GAMEso");
+    }
+    public void PlayGameLA()
+    {
+        SceneManager.LoadScene("GAMEla");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
