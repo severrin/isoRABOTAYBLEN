5a7d6918ac74c23b39beab00684fd389a3bf8606
Severin <h.severrin@gmail.com>
2020-06-21 18:37:36 +0300

    la la la

---
+++ Assets/Scripts/aController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aController : MonoBehaviour
+{
+    public float speed = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //если написать Input.GetAxisRaw - он будет только 0 или 1 (не будет инерции)
+        float verticalMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position += new Vector3(horizontalMove, verticalMove, 0f);
+    }
+}
